c3010075e75b2b0072f742d76f793972ef850275
ateverz <alejandro.tevera@cinvestav.mx>
2024-10-09 19:15:44 +0200

    Add a function to adquire one signal from Simulink output

--- plotting/getFromSimulinkLoggingOutput.m
+++ plotting/getFromSimulinkLoggingOutput.m
@@ -1,7 +1,7 @@
 function signal = getFromSimulinkLoggingOutput(simulationOutput, name)
     % GETFROMSIMULINKLOGGINGOUTPUT Get one logged signal from Simulink. 
-    % SIGNAL = GETFROMSIMULINKLOGGINGOUTPUT(simulationOutput, name) Adquire
-    % the signal with its name from the simulationOutput file. 
+    %   SIGNAL = GETFROMSIMULINKLOGGINGOUTPUT(simulationOutput, name) 
+    %            adquires the 'name' signal from the 'simulationOutput' file. 
     signal_ = find(simulationOutput.logsout, name).Values.Data;
     signal = squeeze(signal_);
     [n,m] = size(signal);
